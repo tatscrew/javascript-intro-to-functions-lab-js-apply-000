c13164c2ab056e9da62ba66101501e8826fbfcea
tatiana.mojica@bustle.com <tatiana.mojica@bustle.com>
2018-08-17 16:54:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-describe('shout(string)', function() {
-  it('receives one argument and returns it all in caps'), function () {
-  expect(shout('hello')) string.toUpperCase ()
+function shout(string) {
+  return `${string.toUpperCase}` 
 }
